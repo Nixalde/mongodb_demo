ce954f19a1238e28557840b109a1ddb97fe2408c
Nixalde <43638580+Nixalde@users.noreply.github.com>
2018-11-11 18:14:54 +0100

    Update mongodb_demo.py

--- mongodb_demo.py
+++ mongodb_demo.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
+from bottle import route, run, request, redirect, post, get, template, static_file, error
+import requests
+import json
 import pymongo
+from sys import argv
+import os
+mykey=os.environ["KEY"]
+
+@get('/')
+def index():
+return template('Index.tpl')
+
 myclient = pymongo.MongoClient('mongodb://172.22.200.178:27017/')
 mydb = myclient["test"]
 mycol = mydb["personal"]
 
 for x in mycol.find():
-  print(x)
+  return (x)
+
+@error(404)
+def error404(error):
+    return 'Data not found'
+
+run(host='0.0.0.0', port=argv[1])

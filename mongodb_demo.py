0bcfcd7db70cf8585dbd42c60dc536552f36a1f8
Nixalde <43638580+Nixalde@users.noreply.github.com>
2018-11-11 22:53:12 +0100

    Update mongodb_demo.py
    Se cambia el uso de bottle por flask

--- mongodb_demo.py
+++ mongodb_demo.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
-from bottle import route, run, request, redirect, post, get, template, static_file, error
-import requests
-import json
+from flask import Flask,render_template,request
 import pymongo
-from sys import argv
+from pymongo import MongoClient
 import os
-mykey=os.environ["KEY"]
+app = Flask(__name__)
 
-@get('/')
-def index():
-return template('Index.tpl')
+@app.route('/')
+def index(query=None):
+	return render_template('Index.html',query=query)
 
-myclient = pymongo.MongoClient('mongodb://172.22.200.178:27017/')
-mydb = myclient["test"]
-mycol = mydb["personal"]
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method=="POST":
+		db_usuario = request.form['name']
+		db_pass = request.form['pass']
+		db_base = request.form['Base']
+		db_coleccion = request.form['Coleccion']
+		myclient = MongoClient('mongodb://172.22.200.178:27017/')
+#		Este es el metodo de autentificacion por usuario
+#		pero no consigue autenticar en el servidor.
+#		myclient = MongoClient('mongodb://%s:%s@172.22.200.178:27017/' % (db_usuario,db_pass))
+		mydb = myclient[db_base]
+		mycol = mydb[db_coleccion]
+		query = mycol.find({})
+		if query == None:
+			error="No es posible realizar la conexión."
+			return render_template("Index.html",query=None)
+		else:
+			return render_template("Index.html",query=query)
+	else:
+		return render_template("Index.html")
 
-for x in mycol.find():
-  return (x)
-
-@error(404)
-def error404(error):
-    return 'Data not found'
-
-run(host='0.0.0.0', port=argv[1])
+app.run('0.0.0.0', port=8080, debug=False)
